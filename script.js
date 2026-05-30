e278da9f87646a2e4b4b847364a30d12b2b58fed
Pendem Rudra krishna <168881482+Rudra7867@users.noreply.github.com>
2026-05-30 19:00:50 +0530

    Connected frontend to render backend

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ async function sendMessage() {
 
         console.log("Sending:", userText);
 
-        const response = await fetch("http://127.0.0.1:5000/chat", {
+        const response = await fetch(
+    "https://hr-rag-chatbot.onrender.com/chat",
+{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
